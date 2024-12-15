252fa9b0228a0ec81d4dc8dd9cd73dd09142f174
Noah Haggerty <haggertynoah@gmail.com>
2024-12-14 16:26:07 -0800

    Adding simple header and footer

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const footer = document.querySelector('footer');
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            footer.innerHTML = data; 
+        });
+});
---
+++ header.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            header.innerHTML = data; 
+        });
+});
